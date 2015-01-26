2c06889c2e699bfb69e538e6c611c8fa7605bc35
Pauline Barmby <pbarmby@uwo.ca>
2015-01-26 16:02:41 -0500

    deleted commented-out lines

--- Fits_silicate.py
+++ Fits_silicate.py
@@ -1,23 +1,7 @@
-#import astropy
-#from astropy.io import fits
-#import montage_wrapper as montage
 import aplpy
-#import pyregion
 import matplotlib.pyplot as plt
 
-#Plots the silicate emission from the nucleus of M31.
-
-#fig, ax=plt.subplots()
-#cax = ax.imshow(pred,origin='lower',extent=xy_extent,interpolation='none',aspect=aspect_num)
-#plt.rcParams['lines.marker']= 'None' # colorbar needs solid borders and no markers                                                                
-#plt.rcParams['lines.linestyle'] = 'solid'
-#cbar=fig.colorbar(cax)
-#plt.rcParams['lines.marker']= 's' # now change back to default                                                                                     
-#plt.rcParams['lines.linestyle'] = 'None'
-
-#gc = aplpy.FITSFigure('silicate2.fits', subplot=(1, 1, 1), north=True) #
 gc = aplpy.FITSFigure('silicate2.fits', subplot=(1, 1, 1), north=True) #
-#gc.show_regions('CentreNuc.reg') # upload some regions in the ds9 format
 
 # the following are just some format options
 gc.axis_labels.set_font(size='x-large', weight='medium', stretch='normal', family='sans-serif', style='normal', variant='normal')
@@ -29,9 +13,6 @@ gc.ticks.set_linewidth(2)  # points
 gc.ticks.set_color('black')
 gc.ticks.set_minor_frequency(5)
 plt.rcParams['lines.marker']= 'None' # colorbar needs solid borders and no markers 
-#
-# overplot some contours
-#gc.show_contour('images/RS250/Hspire250_6.fits', colors='magenta', levels=[0.13, 0.09, 0.06, 0.025, 0.010], linestyles='solid', linewidths=2, returnlevels=True)
 #
 gc.show_colorscale(cmap='YlGnBu', smooth=None, stretch='linear', pmin=50) # the color of the map
 #
@@ -52,7 +33,6 @@ gc.scalebar.set_label('100pc')
 gc.scalebar.set_font(size='x-large', weight='medium', stretch='normal', family='sans-serif', style='normal', variant='normal')
 gc.scalebar.set(linestyle='solid', color='black', linewidth=3)
 #
-#gc.add_label(0.15, 0.85, 'Contours', color=[0.75, 0.25, 0.75], size='x-large', family='sans-serif', relative=True)
 gc.add_label(0.15, 0.9, 'IRS SL1', size='x-large', family='sans-serif', relative=True)
 #
-#gc.save('Output/RS250/W4_6.eps')
+
