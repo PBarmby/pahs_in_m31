b7c2996c5010c1d33f4f24a3730fabb4ce22e7d8
Pauline Barmby <pbarmby@uwo.ca>
2015-02-06 13:21:06 -0500

    separate calculate stuff from plot stuff

--- eqw_plots.py
+++ eqw_plots.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 import math
 
+##### SECTION: calculate stuff
+
 def compute_rhi(atomic_lines, atomic_line_unc):
     """ Calculate single RHI value
     Inputs: atomic_lines: tuple with SIV,SIII,NeIII, NeII fluxes
@@ -46,9 +48,6 @@ def compute_rhi(atomic_lines, atomic_line_unc):
    
     return(RHI,RHI_unc)
 
-
-
-
 def getIIerrorforEngel(NeII,NeIII,SIII,SIV,NeIIval,NeIIIval,SIIIval,SIVval,IIval):
     """ Calculates uncertainties for II, NeIII/NeII and SIV/SIII
     Inputs : first four parameters are the uncertainties of atomic lines and next four are their values. IIval has II values
@@ -74,105 +73,6 @@ def getIIforEngel():
     II = (np.log10((np.array([NeIII_NeII]))) + ( 0.71 + (1.58*(np.log10(np.array(SIV_SIII))))))/2
     meanII = mean(II)
     return (II)
-
-
-def make_fig_10_plot():
-    II = getIIforEngel()
-    np.savetxt('II_values.txt', II)
-    EQW= np.loadtxt("englbrt II") # Here EQW does not have onlt EQWs, it also has atomic lines and uncertainties.
-                                  # Order of the columns are the same as table 5 in Engelbracht et al 200
-    
-    # X is II values and Y is EQWs. These are going to be X and Y axis of the plot.
-    X= np.loadtxt("II_values.txt")
-    
-    Y = list(EQW[0:,0])
-    Yerr = list(EQW[0:,1])
-    Yerr = (np.array(Yerr)/np.array(Y))*0.434
-    Y = [np.log10(a) for a in Y]
-    
-    IIerr = getIIerrorforEngel(EQW[:,3],EQW[:,5], EQW[:,7],EQW[:,9],EQW[:,2],EQW[:,4], EQW[:,6],EQW[:,8], X)
-    Xerr = IIerr
-    plt.errorbar(X,Y,Yerr,Xerr,'ks',linewidth=2.0)
-    plt.plot(X,Y,'ks',label = 'Engelbracht et al. 2008', markersize=15 , mfc = 'white')
-       
-    feature = 0
-    X1,X1err,Y1,Y1err = getIIvsEQW(feature)
-    plt.plot(X1[1], Y1[1], 'b<', markersize=20,linewidth=2.0) #Upper Limit
-    plt.errorbar(X1[3], Y1[3] ,Y1err[3],X1err[3], 'bo', markersize=15,linewidth=2.0)
-    plt.plot(X1[4], Y1[4] , 'b>', markersize=20,linewidth=2.0)
-    plt.errorbar(X1[5], Y1[5] ,Y1err[5],X1err[5], 'bo', markersize=15,linewidth=2.0)
-    plt.errorbar(X1[6], Y1[6] ,Y1err[6],X1err[6], 'bo', markersize=15,linewidth=2.0)
-    plt.plot(X1[7], Y1[7] , 'b<', markersize=20,linewidth=2.0) # Lowe limit
-    plt.errorbar(X1[8], Y1[8] ,Y1err[8],X1err[8], 'bo', markersize=15,linewidth=2.0)
-    plt.plot(X1[8], Y1[8] , 'bo', markersize=15,linewidth=2.0, label = 'M31')
-    
-    plt.xlabel("Log(RHI)", fontsize = 30)
-    plt.ylabel("Log(EQW_8($\mu m$))", fontsize = 30)
-    
-    plt.legend( loc='lower left',prop={'size':20} )
-    
-    plt.yticks([-0.4,-0.2,0,0.2,0.4,0.6,0.8,1.0,1.2])
-    
-    ax = plt.subplot(111)
-    minorLocator   = AutoMinorLocator(5)
-    ax.xaxis.set_minor_locator( minorLocator)
-    
-    plt.tick_params(which='both', width=2)
-    plt.tick_params(which='major', length=10)
-    plt.tick_params(which='minor', length=7, color='k')
-    plt.show()
-    return
-
-
-def plotting(feature,panel,Ylabel):
-    """ Plotting EQWs vs II for M31 regions and also over plotting it with Gordon et al. 2008.
-    This reads the fnction fig8Gordon() in gordonII code.
-
-    X values : II values
-    Y values : EQWs    """ 
-
-    X1,X1err,Y1,Y1err = getIIvsEQW(feature)  # M31 data
-    X2,X2err,Y2,Y2err = fig8Gordon(feature)  # Gordon's data
-
-    axes[panel].errorbar(X2,Y2,Y2err,X2err,'ks', markersize=15,linewidth=2.0)
-    axes[panel].plot(X2,Y2,'gs', markersize=15,linewidth=2.0, mfc = "white", label = 'Gordon et al. 2008')
-
-    axes[panel].plot(X1[1], Y1[1], 'b<', markersize=20,linewidth=2.0) #Upper Limit
-    axes[panel].errorbar(X1[3], Y1[3] ,Y1err[3],X1err[3], 'bo', markersize=15,linewidth=2.0)
-    axes[panel].plot(X1[4], Y1[4], 'b>', markersize=20,linewidth=2.0)
-    axes[panel].errorbar(X1[5], Y1[5] ,Y1err[5],X1err[5], 'bo', markersize=15,linewidth=2.0)
-    axes[panel].errorbar(X1[6], Y1[6] ,Y1err[6],X1err[6], 'bo', markersize=15,linewidth=2.0)
-    axes[panel].plot(X1[7], Y1[7] ,'b<', markersize=20,linewidth=2.0)
-    axes[panel].errorbar(X1[8], Y1[8] ,Y1err[8],X1err[8], 'bo', markersize=15,linewidth=2.0)
-    axes[panel].plot(X1[8], Y1[8] , 'bo', markersize=15,linewidth=2.0, label = 'M31')
-
-    axes[panel].set_ylabel(Ylabel,fontsize=24)
-    axes[1].set_xlabel("Log(RHI)",fontsize=24)
-    axes[panel].tick_params(axis='both', which='major', labelsize=30)
-    axes[panel].tick_params(axis='both', which='minor', labelsize=30)
-    minorLocator   = AutoMinorLocator(5)
-    axes[1].xaxis.set_minor_locator(minorLocator)
-    axes[0].xaxis.set_minor_locator(minorLocator)
-    axes[1].tick_params(which='both', width=2)
-    axes[0].tick_params(which='both', width=2)
-    axes[1].tick_params(which='major', length=10)
-    axes[0].tick_params(which='major', length=10)
-    axes[1].tick_params(which='minor', length=7, color='k')
-    axes[0].tick_params(which='minor', length=7, color='k')
-    
-
-    axes[panel].legend( loc='lower left',prop={'size':20} )
-
-    plt.show()
-    return
-
-
-def make_figure_11():    
-    fig,axes = plt.subplots(2,1,sharex=True)
-    
-    plotting(2,0,"Log(EQW_7.7($\mu m$))")
-    plotting(6,1,"Log(EQW_11.3($\mu m$))")
-    return
 
 def getIIerrorforGordon(NeII,NeIII,SIII,SIV,NeIIval,NeIIIval,SIIIval,SIVval):
     """ Calculates uncertainties for II, NeIII/NeII and SIV/SIII
@@ -236,6 +136,107 @@ def norm_gordon():
         i = i + 2
     np.savetxt('Norm_Gord_EQW.txt', eqw,fmt='%.2f')
     return
+
+
+# SECTION: make plots
+
+def make_fig_10_plot():
+    II = getIIforEngel()
+    np.savetxt('II_values.txt', II)
+    EQW= np.loadtxt("englbrt II") # Here EQW does not have onlt EQWs, it also has atomic lines and uncertainties.
+                                  # Order of the columns are the same as table 5 in Engelbracht et al 200
+    
+    # X is II values and Y is EQWs. These are going to be X and Y axis of the plot.
+    X= np.loadtxt("II_values.txt")
+    
+    Y = list(EQW[0:,0])
+    Yerr = list(EQW[0:,1])
+    Yerr = (np.array(Yerr)/np.array(Y))*0.434
+    Y = [np.log10(a) for a in Y]
+    
+    IIerr = getIIerrorforEngel(EQW[:,3],EQW[:,5], EQW[:,7],EQW[:,9],EQW[:,2],EQW[:,4], EQW[:,6],EQW[:,8], X)
+    Xerr = IIerr
+    plt.errorbar(X,Y,Yerr,Xerr,'ks',linewidth=2.0)
+    plt.plot(X,Y,'ks',label = 'Engelbracht et al. 2008', markersize=15 , mfc = 'white')
+       
+    feature = 0
+    X1,X1err,Y1,Y1err = getIIvsEQW(feature)
+    plt.plot(X1[1], Y1[1], 'b<', markersize=20,linewidth=2.0) #Upper Limit
+    plt.errorbar(X1[3], Y1[3] ,Y1err[3],X1err[3], 'bo', markersize=15,linewidth=2.0)
+    plt.plot(X1[4], Y1[4] , 'b>', markersize=20,linewidth=2.0)
+    plt.errorbar(X1[5], Y1[5] ,Y1err[5],X1err[5], 'bo', markersize=15,linewidth=2.0)
+    plt.errorbar(X1[6], Y1[6] ,Y1err[6],X1err[6], 'bo', markersize=15,linewidth=2.0)
+    plt.plot(X1[7], Y1[7] , 'b<', markersize=20,linewidth=2.0) # Lowe limit
+    plt.errorbar(X1[8], Y1[8] ,Y1err[8],X1err[8], 'bo', markersize=15,linewidth=2.0)
+    plt.plot(X1[8], Y1[8] , 'bo', markersize=15,linewidth=2.0, label = 'M31')
+    
+    plt.xlabel("Log(RHI)", fontsize = 30)
+    plt.ylabel("Log(EQW_8($\mu m$))", fontsize = 30)
+    
+    plt.legend( loc='lower left',prop={'size':20} )
+    
+    plt.yticks([-0.4,-0.2,0,0.2,0.4,0.6,0.8,1.0,1.2])
+    
+    ax = plt.subplot(111)
+    minorLocator   = AutoMinorLocator(5)
+    ax.xaxis.set_minor_locator( minorLocator)
+    
+    plt.tick_params(which='both', width=2)
+    plt.tick_params(which='major', length=10)
+    plt.tick_params(which='minor', length=7, color='k')
+    plt.show()
+    return
+
+def make_figure_11():    
+    fig,axes = plt.subplots(2,1,sharex=True)
+    
+    plotting(2,0,"Log(EQW_7.7($\mu m$))")
+    plotting(6,1,"Log(EQW_11.3($\mu m$))")
+    return
+
+
+def plotting(feature,panel,Ylabel):
+    """ Plotting EQWs vs II for M31 regions and also over plotting it with Gordon et al. 2008.
+    Uses the fnction fig8Gordon() 
+
+    X values : II values
+    Y values : EQWs    """ 
+
+    X1,X1err,Y1,Y1err = getIIvsEQW(feature)  # M31 data
+    X2,X2err,Y2,Y2err = fig8Gordon(feature)  # Gordon's data
+
+    axes[panel].errorbar(X2,Y2,Y2err,X2err,'ks', markersize=15,linewidth=2.0)
+    axes[panel].plot(X2,Y2,'gs', markersize=15,linewidth=2.0, mfc = "white", label = 'Gordon et al. 2008')
+
+    axes[panel].plot(X1[1], Y1[1], 'b<', markersize=20,linewidth=2.0) #Upper Limit
+    axes[panel].errorbar(X1[3], Y1[3] ,Y1err[3],X1err[3], 'bo', markersize=15,linewidth=2.0)
+    axes[panel].plot(X1[4], Y1[4], 'b>', markersize=20,linewidth=2.0)
+    axes[panel].errorbar(X1[5], Y1[5] ,Y1err[5],X1err[5], 'bo', markersize=15,linewidth=2.0)
+    axes[panel].errorbar(X1[6], Y1[6] ,Y1err[6],X1err[6], 'bo', markersize=15,linewidth=2.0)
+    axes[panel].plot(X1[7], Y1[7] ,'b<', markersize=20,linewidth=2.0)
+    axes[panel].errorbar(X1[8], Y1[8] ,Y1err[8],X1err[8], 'bo', markersize=15,linewidth=2.0)
+    axes[panel].plot(X1[8], Y1[8] , 'bo', markersize=15,linewidth=2.0, label = 'M31')
+
+    axes[panel].set_ylabel(Ylabel,fontsize=24)
+    axes[1].set_xlabel("Log(RHI)",fontsize=24)
+    axes[panel].tick_params(axis='both', which='major', labelsize=30)
+    axes[panel].tick_params(axis='both', which='minor', labelsize=30)
+    minorLocator   = AutoMinorLocator(5)
+    axes[1].xaxis.set_minor_locator(minorLocator)
+    axes[0].xaxis.set_minor_locator(minorLocator)
+    axes[1].tick_params(which='both', width=2)
+    axes[0].tick_params(which='both', width=2)
+    axes[1].tick_params(which='major', length=10)
+    axes[0].tick_params(which='major', length=10)
+    axes[1].tick_params(which='minor', length=7, color='k')
+    axes[0].tick_params(which='minor', length=7, color='k')
+    
+
+    axes[panel].legend( loc='lower left',prop={'size':20} )
+
+    plt.show()
+    return
+
 
 # Feature = which feature u want to plot with II. 0 = 6.2, 2 = 7.7, 4 = 8.6, 6 = 11.3, 8 = 12.7
 def fig8Gordon(feature):
