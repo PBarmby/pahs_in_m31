abfa9ac6714deb56a2c2f8b0cb1193e0113da53c
Pauline Barmby <pbarmby@uwo.ca>
2015-02-09 11:40:12 -0500

    Figure 10 function runs

--- eqw_plots.py
+++ eqw_plots.py
@@ -86,46 +86,46 @@ def process_m31_data():
 
 # SECTION: make plots
 def make_fig_10_plot(engel_tab, m31_tab):
+    fix, ax = plt.subplots()
     # plot Engelbracht data
-    Y= engel_tab('PAH8')
-    Yerr = engel_tab('PAH8_unc')    
+    Y= engel_tab['PAH8eqw']
+    Yerr = engel_tab['PAH8eqw_unc']    
     Yerr = (Yerr/Y)*0.434
     Y = np.log10(Y)
-    X = engel_tab('RHI')
-    Xerr = engel_tab('RHI_unc')
-    plt.errorbar(X,Y,Yerr,Xerr,'ks',linewidth=2.0)
-    plt.plot(X,Y,'ks',label = 'Engelbracht et al. 2008', markersize=15 , mfc = 'white')
+    X = engel_tab['RHI']
+    Xerr = engel_tab['RHI_unc']
+    ax.errorbar(X,Y,Yerr,Xerr,'ks',linewidth=2.0)
+    ax.plot(X,Y,'ks',label = 'Engelbracht et al. 2008', markersize=15 , mfc = 'white')
        
     # plot M31 data
-    X1,X1err,Y1,Y1err = m31tab['RHI'], m31tab['RHI_unc'], m31tab['PAH8'], m31tab['PAH8_unc'] # NB: PAH8 = 7.7+8.3+8.6, may need to add
-    plt.plot(X1[1], Y1[1], 'b<', markersize=20,linewidth=2.0) #Upper Limit
-    plt.errorbar(X1[3], Y1[3] ,Y1err[3],X1err[3], 'bo', markersize=15,linewidth=2.0)
-    plt.plot(X1[4], Y1[4] , 'b>', markersize=20,linewidth=2.0)
-    plt.errorbar(X1[5], Y1[5] ,Y1err[5],X1err[5], 'bo', markersize=15,linewidth=2.0)
-    plt.errorbar(X1[6], Y1[6] ,Y1err[6],X1err[6], 'bo', markersize=15,linewidth=2.0)
-    plt.plot(X1[7], Y1[7] , 'b<', markersize=20,linewidth=2.0) # Lowe limit
-    plt.errorbar(X1[8], Y1[8] ,Y1err[8],X1err[8], 'bo', markersize=15,linewidth=2.0)
-    plt.plot(X1[8], Y1[8] , 'bo', markersize=15,linewidth=2.0, label = 'M31')
+    X1,X1err,Y1,Y1err = m31_tab['RHI'], m31_tab['RHI_unc'], np.log10(m31_tab['PAH8eqw']), 0.434*(m31_tab['PAH8eqw_unc']/m31_tab['PAH8eqw'])
+    ax.plot(X1[1], Y1[1], 'b<', markersize=20,linewidth=2.0) #Upper Limit
+    ax.errorbar(X1[3], Y1[3] ,Y1err[3],X1err[3], 'bo', markersize=15,linewidth=2.0)
+    ax.plot(X1[4], Y1[4] , 'b>', markersize=20,linewidth=2.0)
+    ax.errorbar(X1[5], Y1[5] ,Y1err[5],X1err[5], 'bo', markersize=15,linewidth=2.0)
+    ax.errorbar(X1[6], Y1[6] ,Y1err[6],X1err[6], 'bo', markersize=15,linewidth=2.0)
+    ax.plot(X1[7], Y1[7] , 'b<', markersize=20,linewidth=2.0) # Lowe limit
+    ax.errorbar(X1[8], Y1[8] ,Y1err[8],X1err[8], 'bo', markersize=15,linewidth=2.0)
+    ax.plot(X1[8], Y1[8] , 'bo', markersize=15,linewidth=2.0, label = 'M31')
     
     # plot formatting
-    plt.xlabel("Log(RHI)", fontsize = 30)
-    plt.ylabel("Log(EQW_8($\mu m$))", fontsize = 30)
-    plt.legend( loc='lower left',prop={'size':20} )
-    plt.yticks([-0.4,-0.2,0,0.2,0.4,0.6,0.8,1.0,1.2])
-    ax = plt.subplot(111)
-    minorLocator   = AutoMinorLocator(5)
-    ax.xaxis.set_minor_locator( minorLocator)
-    plt.tick_params(which='both', width=2)
-    plt.tick_params(which='major', length=10)
-    plt.tick_params(which='minor', length=7, color='k')
-    plt.show()
+    ax.set_xlabel("Log(RHI)", fontsize = 20)
+    ax.set_ylabel("Log(8$\mu m$ EQW)", fontsize = 20)
+#    ax.legend(loc='best',prop={'size':20} )
+#    ax.yticks([-0.4,-0.2,0,0.2,0.4,0.6,0.8,1.0,1.2])
+#    minorLocator   = AutoMinorLocator(5)
+#    ax.xaxis.set_minor_locator( minorLocator)
+#    ax.tick_params(which='both', width=2)
+#    ax.tick_params(which='major', length=10)
+#    ax.tick_params(which='minor', length=7, color='k')
+    plt.draw()
     return
 
 def make_figure_11(m31dat, gord_dat):    
     fig,axes = plt.subplots(2,1,sharex=True)
     
-    plotting(m31dat, gord_dat, 'PAH7.7',0,"Log(EQW_7.7($\mu m$))")
-    plotting(m31dat, gord_dat, 'PAH11.3',1,"Log(EQW_11.3($\mu m$))")
+    plotting(m31dat, gord_dat, 'PAH7.7',0,"Log(7.7$\mu m$ EQW)")
+    plotting(m31dat, gord_dat, 'PAH11.3',1,"Log(11.3$\mu m$ EQW)")
     return
 
 
