03d0bdcb3065f20774c239967138a6eecf3901fd
Pauline Barmby <pbarmby@uwo.ca>
2015-08-25 15:01:39 -0400

    started work toward changing how inset figure done

--- pb_nucleus/nucCUBsmith.py
+++ pb_nucleus/nucCUBsmith.py
@@ -22,6 +22,7 @@ from matplotlib import ticker
 from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, \
      AnnotationBbox
 from matplotlib._png import read_png
+from mpl_toolkits.axes_grid.inset_locator import inset_axes
 
 # original values
 #spec_mult_offsets = {'M31':1.0, 'NGC1316':12.0, 'NGC4594':14.0, 'NGC1404':14.0, 'NGC2841':10.0, 'NGC4552':10.0, 'NGC4125':8.0}
@@ -192,18 +193,22 @@ def doplot(color=True):
     ########################################################################################
     # This section inserts the subplot from silicate.py in to the main plot.
 #    fn = get_sample_dat("./silicate.png", asfileobj=False)
-    if color:
-        arr_lena = read_png("./silicate.png")
-        imagebox = OffsetImage(arr_lena, zoom=0.4)
-    else:
-        arr_lena = read_png("./silicate_bw.png")
-        imagebox = OffsetImage(arr_lena, zoom=0.25)        
-    
-    ab = AnnotationBbox(imagebox, xy=(17, 6),
-                            xycoords='data',
-                            boxcoords="offset points")
-                            
-    ax.add_artist(ab)
+
+#    if color:
+#        arr_lena = read_png("./silicate.png")
+#        imagebox = OffsetImage(arr_lena, zoom=0.4)
+#    else:
+#        arr_lena = read_png("./silicate_bw.png")
+#        imagebox = OffsetImage(arr_lena, zoom=0.25)        
+#    
+#    ab = AnnotationBbox(imagebox, xy=(17, 6),
+#                            xycoords='data',
+#                            boxcoords="offset points")
+#                            
+#    ax.add_artist(ab)
+
+    inax = inset_axes(ax, width = "33%" , height = "50%", loc = )
+    inax.
     
 #   legend, formatting
     
@@ -226,3 +231,92 @@ def doplot(color=True):
     plt.show()
     return
 
+""" This code plots spectra extracted from the crntre of the nucleus and the north region of M31
+in two panels.
+You should have .tbl files obtained from CUBISM for both regions to run this code."""
+
+
+def getspectrum(sl1,sl2,ll2):
+    # Read spectra from 3 modules and stitch them.
+    # Returns the final stitched spectra
+
+    W_SL2,F_SL2,SL2_unc  = sl2[:,0] , sl2[:,1],sl2[:,2]
+    W_SL1,F_SL1,SL1_unc  = sl1[:,0] , sl1[:,1], sl1[:,2]
+    W_LL2,F_LL2,LL2_unc  = ll2[:,0] , ll2[:,1], ll2[:,2]
+    
+    Y = []
+    X = []
+    Yerr = []
+
+
+    # The below For loops cut the spectra at specific wavelengths and merge them into a
+    # one array. Cutting points were determined by observing the unstitched spectrum.
+    # It adds wavelengths in to X, Flux into Y and uncertainty into Yerr.
+    # Note : We ignored SL3 spectrum
+
+    for (i,aval) in enumerate(W_SL2):
+        if aval < 7.57 :
+            Y.append(F_SL2[i])
+            X.append(aval)
+            Yerr.append(SL2_unc[i])
+      
+    for (i,aval) in enumerate(W_SL1):
+        if (aval > 7.57) & (aval < 14.50):
+            Y.append(F_SL1[i])
+            X.append(aval)
+            Yerr.append(SL1_unc[i])
+
+    for (i,aval) in enumerate(W_LL2):
+        if aval > 14.50 :
+            Y.append(F_LL2[i])
+            X.append(aval)
+            Yerr.append(LL2_unc[i])
+
+    return X,Y,Yerr
+
+
+def plotting(X,Y,Yerr,panel,axes,col=True):
+
+    # Plot spectra into panels.
+    if col:
+        axes[panel].errorbar(X,Y,Yerr,0,'-')
+    else:
+        axes[panel].errorbar(X,Y,Yerr,0,'-', color='k')
+    axes[panel].tick_params(axis='both', which='major', labelsize=24)
+    axes[panel].tick_params(axis='both', which='minor', labelsize=24)
+
+def doplot(color=True, axes):
+    matplotlib.rcdefaults()
+#    fig,axes = plt.subplots(2,1,sharex=True)
+    minorLocator = AutoMinorLocator(5)
+    for ax in axes:
+        ax.xaxis.set_minor_locator(minorLocator)
+        ax.tick_params(which='both', width=2)
+        ax.tick_params(which='major', length=10)
+        ax.tick_params(which='minor', length=7, color='k')
+        ax.tick_params(which='both',labelsize=24)
+        ax.tick_params(axis='both', which='major', labelsize=24)
+        ax.tick_params(axis='both', which='minor', labelsize=24)
+    ax[0].text(0.03, 0.5, 'Intensity (MJy/sr)', ha='center', va='center', rotation='vertical',fontsize=30)
+    
+    # Reading .tbl files got from CUBISM 
+    sl2 = np.loadtxt("m31nuc_sl2_nucCentre.tbl", skiprows = 15 )
+    sl1 = np.loadtxt("m31nuc_sl1_nucCentre.tbl", skiprows = 15 )
+    ll2 = np.loadtxt("m31nuc_ll2_nucCentre.tbl", skiprows = 15 )
+    Wavelength,Flux,FluxUnc = getspectrum(sl1,sl2,ll2)
+    
+    plotting(Wavelength,Flux,FluxUnc,1,axes,col=color)
+    axes[1].set_xlabel("Wavelength ($\mu m$)",fontsize=24)
+    axes[1].annotate('Silicate', xy=(9.7, 50),  xycoords='data',xytext=None,size=30, textcoords='offset points',arrowprops=dict(arrowstyle="->", linewidth = 4))
+    axes[1].set_ylim(10,88)
+    axes[1].yaxis.set_major_locator(MultipleLocator(20))    
+
+    Wavelength,Flux,FluxUnc = np.loadtxt('m31nuc_nucUP_correct.dat',usecols=(0,1,2),unpack=True)
+    
+    plotting(Wavelength,Flux,FluxUnc,0,axes,col=color)
+    axes[0].set_ylim(9,45)
+    axes[0].yaxis.set_major_locator(MultipleLocator(10))
+
+#    fig.subplots_adjust(left=0.15, bottom=0.16, hspace=0.0)
+#    plt.show()
+    return
